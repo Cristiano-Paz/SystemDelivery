6d81e95b9ee2e2ac73240fd59c505c0d6b03c85d
Cristiano-Paz <cristianogspaz@gmail.com>
2025-09-13 16:24:24 -0300

    commit inicial

---
+++ Microservice/Courier-Management/src/test/java/com/crisdev/systemdelivery/courier/management/CourierManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crisdev.systemdelivery.courier.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
